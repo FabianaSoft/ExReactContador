4535d01816ba04ade05793bba94b77ccfa70ee57
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-07-05 15:32:27 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 
 
 function App() {
+  const [contador, setContador] = useState(0);
+  function decrementar() {
+    setContador(contador - 1);
+  }
+  function reset() {
+    setContador(0);
+  }
+
+  function incrementar() {
+    setContador(contador + 1);
+  }
+
   return (
-    <>contador</>
+    <>
+      <h1>Contador:</h1>
+      <h2>{contador}</h2>
+      <button onClick={decrementar}>&lt; Contador</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={incrementar}>&gt; Contador</button>
+
+
+
+
+
+    </>
   );
 }
 
